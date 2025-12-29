855451e2f03293881a2bb22c560087f52f6bc204
Patience Moffat <moffatpatience37@gmail.com>
2025-12-29 19:58:21 +0000

    Remove sandbox and use production

--- src/lib/flutterwave-config.ts
+++ src/lib/flutterwave-config.ts
@@ -1,33 +1,21 @@
 
-const isProd = process.env.NODE_ENV === 'production';
-
 // URLs
-const SANDBOX_AUTH_URL = 'https://idp-sandbox.flutterwave.com/oauth2/token';
 const PROD_AUTH_URL = 'https://idp.flutterwave.com/realms/flutterwave/protocol/openid-connect/token';
-
-const SANDBOX_API_URL = 'https://api.flutterwave.com/v3';
 const PROD_API_URL = 'https://api.flutterwave.com/v3';
 
 
 // --- SERVER-SIDE CONFIG ---
 // These values are only accessible on the server.
-export const FLW_AUTH_URL = isProd ? PROD_AUTH_URL : SANDBOX_AUTH_URL;
-export const FLW_API_URL = isProd ? PROD_API_URL : SANDBOX_API_URL;
+export const FLW_AUTH_URL = PROD_AUTH_URL;
+export const FLW_API_URL = PROD_API_URL;
 
-export const FLW_CLIENT_ID = isProd
-  ? process.env.FLW_PROD_CLIENT_ID
-  : process.env.FLW_SANDBOX_CLIENT_ID;
-
-export const FLW_CLIENT_SECRET = isProd
-  ? process.env.FLW_PROD_CLIENT_SECRET
-  : process.env.FLW_SANDBOX_CLIENT_SECRET;
+export const FLW_CLIENT_ID = process.env.FLW_CLIENT_ID;
+export const FLW_CLIENT_SECRET = process.env.FLW_CLIENT_SECRET;
 
 
 // --- CLIENT-SIDE CONFIG ---
 // These values are prefixed with NEXT_PUBLIC_ and are safe to be exposed to the client.
-export const FLW_PUBLIC_ENCRYPTION_KEY = isProd
-  ? process.env.NEXT_PUBLIC_FLW_PROD_ENCRYPTION_KEY
-  : process.env.NEXT_PUBLIC_FLW_SANDBOX_ENCRYPTION_KEY;
+export const FLW_PUBLIC_ENCRYPTION_KEY = process.env.NEXT_PUBLIC_FLW_ENCRYPTION_KEY;
 
 // Warning for missing configuration
 if (!FLW_CLIENT_ID || !FLW_CLIENT_SECRET || !FLW_PUBLIC_ENCRYPTION_KEY) {
